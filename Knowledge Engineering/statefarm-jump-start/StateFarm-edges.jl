585329bb29651ac5a315d8e24ad327242de092fa
ppant239 <38893764+ppant239@users.noreply.github.com>
2022-05-01 19:15:51 -0500

    Updated

--- Knowledge Engineering/statefarm-jump-start/StateFarm-edges.jl
+++ Knowledge Engineering/statefarm-jump-start/StateFarm-edges.jl
@@ -1,53 +1,53 @@
 {"company": "StateFarm", "relation": "sells", "service": "advertising"}
 {"company": "StateFarm", "relation": "sells", "service": "advertising"}
-{"company": "General Motors", "relation": "is_in", "industry": "1    Automotive industry"}
+{"company": "General Motors", "relation": "is_in", "industry": "Automotive industry"}
 {"company": "General Motors", "relation": "buys", "service": "advertising"}
-{"company": "The Hertz Corporation", "relation": "is_in", "industry": "1    Car rental and leasing"}
+{"company": "The Hertz Corporation", "relation": "is_in", "industry": "Car rental and leasing"}
 {"company": "The Hertz Corporation", "relation": "buys", "service": "advertising"}
-{"company": "Chrysler", "relation": "is_in", "industry": "1    Automotive"}
+{"company": "Chrysler", "relation": "is_in", "industry": "Automotive"}
 {"company": "Chrysler", "relation": "buys", "service": "advertising"}
-{"company": "State Farm", "relation": "is_in", "industry": "1    Insurance"}
+{"company": "State Farm", "relation": "is_in", "industry": "Insurance"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "State Farm"}
-{"company": "Berkshire Hathaway", "relation": "is_in", "industry": "1    Conglomerate"}
+{"company": "Berkshire Hathaway", "relation": "is_in", "industry": "Conglomerate"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "Berkshire Hathaway"}
-{"company": "Progressive Corporation", "relation": "is_in", "industry": "1    Insurance"}
+{"company": "Progressive Corporation", "relation": "is_in", "industry": "Insurance"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "Progressive Corporation"}
-{"company": "Liberty Mutual", "relation": "is_in", "industry": "1    Insurance"}
+{"company": "Liberty Mutual", "relation": "is_in", "industry": "Insurance"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "Liberty Mutual"}
-{"company": "Allstate", "relation": "is_in", "industry": "1    Insurance"}
+{"company": "Allstate", "relation": "is_in", "industry": "Insurance"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "Allstate"}
-{"company": "The Travelers Companies", "relation": "is_in", "industry": "1    InsuranceFinancial services"}
+{"company": "The Travelers Companies", "relation": "is_in", "industry": "InsuranceFinancial services"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "The Travelers Companies"}
-{"company": "USAA", "relation": "is_in", "industry": "1    BankingFinancial servicesInsurance"}
+{"company": "USAA", "relation": "is_in", "industry": "BankingFinancial servicesInsurance"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "USAA"}
-{"company": "Farmers Insurance Group", "relation": "is_in", "industry": "1    Financial services"}
+{"company": "Farmers Insurance Group", "relation": "is_in", "industry": "Financial services"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "Farmers Insurance Group"}
-{"company": "Nationwide Mutual Insurance Company", "relation": "is_in", "industry": "1    Financial services"}
+{"company": "Nationwide Mutual Insurance Company", "relation": "is_in", "industry": "Financial services"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "Nationwide Mutual Insurance Company"}
-{"company": "American International Group", "relation": "is_in", "industry": "1    Financial services"}
+{"company": "American International Group", "relation": "is_in", "industry": "Financial services"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "American International Group"}
-{"company": "Zurich Insurance Group", "relation": "is_in", "industry": "1    Financial services"}
+{"company": "Zurich Insurance Group", "relation": "is_in", "industry": "Financial services"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "Zurich Insurance Group"}
-{"company": "The Hartford", "relation": "is_in", "industry": "1    InsuranceMutual funds"}
+{"company": "The Hartford", "relation": "is_in", "industry": "InsuranceMutual funds"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "The Hartford"}
-{"company": "Assurant", "relation": "is_in", "industry": "1    Insurance"}
+{"company": "Assurant", "relation": "is_in", "industry": "Insurance"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "Assurant"}
-{"company": "Erie Insurance Group", "relation": "is_in", "industry": "1    Insurance"}
+{"company": "Erie Insurance Group", "relation": "is_in", "industry": "Insurance"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "Erie Insurance Group"}
-{"company": "Fairfax Financial", "relation": "is_in", "industry": "1    Insurance"}
+{"company": "Fairfax Financial", "relation": "is_in", "industry": "Insurance"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "Fairfax Financial"}
-{"company": "W. R. Berkley Corporation", "relation": "is_in", "industry": "1    Insurance"}
+{"company": "W. R. Berkley Corporation", "relation": "is_in", "industry": "Insurance"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "W. R. Berkley Corporation"}
-{"company": "Markel Corporation", "relation": "is_in", "industry": "1    Insurance"}
+{"company": "Markel Corporation", "relation": "is_in", "industry": "Insurance"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "Markel Corporation"}
-{"company": "Axa", "relation": "is_in", "industry": "1    Financial services"}
+{"company": "Axa", "relation": "is_in", "industry": "Financial services"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "Axa"}
-{"company": "American Family Insurance", "relation": "is_in", "industry": "1    Insurance and finance"}
+{"company": "American Family Insurance", "relation": "is_in", "industry": "Insurance and finance"}
 {"company1": "StateFarm", "relation": "competes_with", "company2": "American Family Insurance"}
-{"company": "Lemonade, Inc.", "relation": "is_in", "industry": "1    Insurance"}
+{"company": "Lemonade, Inc.", "relation": "is_in", "industry": "Insurance"}
 {"company1": "StateFarm", "relation": "has_potential_entrant", "company2": "Lemonade, Inc."}
-{"company": "Root, Inc.", "relation": "is_in", "industry": "1    Insurance"}
+{"company": "Root, Inc.", "relation": "is_in", "industry": "Insurance"}
 {"company1": "StateFarm", "relation": "has_potential_entrant", "company2": "Root, Inc."}
-{"company": "Munich Re", "relation": "is_in", "industry": "1    Financial services"}
-{"company": "Swiss Re", "relation": "is_in", "industry": "1    Financial services"}
-{"company": "Hannover Re", "relation": "is_in", "industry": "1    Financial services"}
+{"company": "Munich Re", "relation": "is_in", "industry": "Financial services"}
+{"company": "Swiss Re", "relation": "is_in", "industry": "Financial services"}
+{"company": "Hannover Re", "relation": "is_in", "industry": "Financial services"}
