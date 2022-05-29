54527a3866658f0c81800b0631a02a85fb79d6c3
ppant239 <38893764+ppant239@users.noreply.github.com>
2022-05-29 17:19:37 -0500

    Updated files for 459 - assignment 3

--- Knowledge Engineering/statefarm-jump-start/StateFarm-nodes-company.jl
+++ Knowledge Engineering/statefarm-jump-start/StateFarm-nodes-company.jl
@@ -1,1 +1,96 @@
-SELECT * FROM cypher('sf_graph', $$ CREATE (:Competitor {type: "company", name: "General Motors", url: "https://www.cadillac.com", attributes: {founded: "September16, 1908; 113 years ago[1] (original...", numberEmployees: "157,000 (December 2021)[2]"}}) $$) as (n agtype);
+{"type": "company", "name": "General Motors", "url": "https://www.cadillac.com", "attributes": {"founded": "September16, 1908; 113 years ago[1] (original...", "numberEmployees": "157,000"}}
+{"type": "company", "name": "The Hertz Corporation", "url": "https://en.wikipedia.org/wiki/The_Hertz_Corporation", "attributes": {"founded": "1918; 104years ago in Chicago, Illinois, U.S.", "numberEmployees": "24,000 (2020)"}}
+{"type": "company", "name": "Chrysler", "url": "https://en.wikipedia.org/wiki/Enterprise_Rent-A-Car", "attributes": {"founded": "June6, 1925; 96 years ago", "numberEmployees": "c. 90,000(2019)"}}
+{"type": "company", "name": "State Farm", "url": "https://www.statefarm.com/", "attributes": {"founded": "June7, 1922; 99 years ago[2]", "numberEmployees": "57,672 (2019)[1]"}}
+{"type": "company", "name": "Berkshire Hathaway", "url": "https://www.berkshirehathaway.com/", "attributes": {"founded": "1839; 183years agoCumberland, Rhode Island, U.S.", "numberEmployees": "372,000 (2021)[2]"}}
+{"type": "company", "name": "Progressive Corporation", "url": "https://www.progressive.com/", "attributes": {"founded": "March10, 1937; 85 years ago", "numberEmployees": "43,000+ (2021)"}}
+{"type": "company", "name": "Liberty Mutual", "url": "https://www.libertymutual.com/", "attributes": {"founded": "July1, 1912; 109 years ago (as Massachusetts ...", "numberEmployees": "45,000+(2021)"}}
+{"type": "company", "name": "Allstate", "url": "https://www.allstate.com/", "attributes": {"founded": "April17, 1931; 91 years ago[1]", "numberEmployees": "45,780 (2019)[2]"}}
+{"type": "company", "name": "The Travelers Companies", "url": "https://www.travelers.com/", "attributes": {"founded": "1853; 169years ago (as St. Paul Fire & Marine...", "numberEmployees": "~30,800 (December 2019)[2]"}}
+{"type": "company", "name": "USAA", "url": "https://www.usaa.com/ ", "attributes": {"founded": "June20, 1922; 99 years ago", "numberEmployees": "36,000 (2020)[6]"}}
+{"type": "company", "name": "Farmers Insurance Group", "url": "https://www.farmers.com/", "attributes": {"founded": "1928; 94years agoLos Angeles,[2] California, ...", "numberEmployees": "12,740 (2019)"}}
+{"type": "company", "name": "Nationwide Mutual Insurance Company", "url": "https://www.nationwide.com", "attributes": {"founded": "1926; 96years ago", "numberEmployees": "25391"}}
+{"type": "company", "name": "American International Group", "url": "https://www.aig.com/business", "attributes": {"founded": "December19, 1919; 102 years ago[1]Shanghai, C...", "numberEmployees": "49,600 (2020)"}}
+{"type": "company", "name": "Zurich Insurance Group", "url": "https://www.zurich.com", "attributes": {"founded": "1872; 150years ago", "numberEmployees": "53,535 (2018)[1]"}}
+{"type": "company", "name": "The Hartford", "url": "https://www.thehartford.com", "attributes": {"founded": "1810; 212years ago", "numberEmployees": "~19,500 (2019)"}}
+{"type": "company", "name": "American Family Insurance", "url": "https://www.assurant.com", "attributes": {"founded": "1927; 95years ago", "numberEmployees": "11,307 [4](2017)"}}
+{"type": "company", "name": "Assurant", "url": "https://www.erieinsurance.com", "attributes": {"founded": "1892; 130years ago", "numberEmployees": "14,250 (2020)[citation needed]"}}
+{"type": "company", "name": "Erie Insurance Group", "url": "https://www.lemonade.com/?f=1", "attributes": {"founded": "April20, 1925; 96 years ago", "numberEmployees": "6,000(2022)"}}
+{"type": "company", "name": "Root, Inc.", "url": "https://www.joinroot.com/", "attributes": {"founded": "March2015", "numberEmployees": "1,241(2022)"}}
+{"type": "company", "name": "Lemonade, Inc.", "url": "https://milestoneins.net/", "attributes": {"founded": "April2015; 7years ago", "numberEmployees": "1,119 (Dec 2021)"}}
+{"type": "company", "name": "General Motors", "url": "https://www.cadillac.com", "attributes": {"founded": "September16, 1908; 113 years ago[1] (original...", "numberEmployees": "157,000 (December 2021)[2]"}}
+{"type": "company", "name": "The Hertz Corporation", "url": "https://en.wikipedia.org/wiki/The_Hertz_Corporation", "attributes": {"founded": "1918; 104years ago in Chicago, Illinois, U.S.", "numberEmployees": "24,000 (2020)"}}
+{"type": "company", "name": "Chrysler", "url": "https://en.wikipedia.org/wiki/Chrysler", "attributes": {"founded": "June6, 1925; 96 years ago", "numberEmployees": "c. 90,000(2019)"}}
+{"type": "company", "name": "State Farm", "url": "https://www.statefarm.com/", "attributes": {"founded": "June7, 1922; 99 years ago[2]", "numberEmployees": "57,672 (2019)[1]"}}
+{"type": "company", "name": "Berkshire Hathaway", "url": "https://www.berkshirehathaway.com/", "attributes": {"founded": "1839; 183years agoCumberland, Rhode Island, U.S.", "numberEmployees": "372,000 (2021)[2]"}}
+{"type": "company", "name": "Progressive Corporation", "url": "https://www.progressive.com/", "attributes": {"founded": "March10, 1937; 85 years ago", "numberEmployees": "43,000+ (2021)"}}
+{"type": "company", "name": "Liberty Mutual", "url": "https://www.libertymutual.com/", "attributes": {"founded": "July1, 1912; 109 years ago (as Massachusetts ...", "numberEmployees": "45,000+(2021)"}}
+{"type": "company", "name": "Allstate", "url": "https://www.allstate.com/", "attributes": {"founded": "April17, 1931; 91 years ago[1]", "numberEmployees": "45,780 (2019)[2]"}}
+{"type": "company", "name": "The Travelers Companies", "url": "https://www.travelers.com/", "attributes": {"founded": "1853; 169years ago (as St. Paul Fire & Marine...", "numberEmployees": "~30,800 (December 2019)[2]"}}
+{"type": "company", "name": "USAA", "url": "https://www.usaa.com/ ", "attributes": {"founded": "June20, 1922; 99 years ago", "numberEmployees": "36,000 (2020)[6]"}}
+{"type": "company", "name": "Farmers Insurance Group", "url": "https://www.farmers.com/", "attributes": {"founded": "1928; 94years agoLos Angeles,[2] California, ...", "numberEmployees": "12,740 (2019)"}}
+{"type": "company", "name": "Nationwide Mutual Insurance Company", "url": "https://www.nationwide.com", "attributes": {"founded": "1926; 96years ago", "numberEmployees": "25391"}}
+{"type": "company", "name": "American International Group", "url": "https://www.aig.com/business", "attributes": {"founded": "December19, 1919; 102 years ago[1]Shanghai, C...", "numberEmployees": "49,600 (2020)"}}
+{"type": "company", "name": "Zurich Insurance Group", "url": "https://www.zurich.com", "attributes": {"founded": "1872; 150years ago", "numberEmployees": "53,535 (2018)[1]"}}
+{"type": "company", "name": "The Hartford", "url": "https://www.thehartford.com", "attributes": {"founded": "1810; 212years ago", "numberEmployees": "~19,500 (2019)"}}
+{"type": "company", "name": "American Family Insurance", "url": "https://www.assurant.com", "attributes": {"founded": "1927; 95years ago", "numberEmployees": "11,307 [4](2017)"}}
+{"type": "company", "name": "Assurant", "url": "https://www.erieinsurance.comhttps://www.fairfax.ca/", "attributes": {"founded": "1892; 130years ago", "numberEmployees": "14,250 (2020)[citation needed]"}}
+{"type": "company", "name": "Erie Insurance Group", "url": "https://www.berkley.com", "attributes": {"founded": "April20, 1925; 96 years ago", "numberEmployees": "6,000(2022)"}}
+{"type": "company", "name": "Markel Corporation", "url": "https://www.markel.com/", "attributes": {"founded": "1930; 92years ago", "numberEmployees": "17,400[2](2019)"}}
+{"type": "company", "name": "Axa", "url": "https://www.axa.com/", "attributes": {"founded": "1816; 206years ago[1]", "numberEmployees": "120,869 (2020)[2]"}}
+{"type": "company", "name": "W. R. Berkley Corporation", "url": "https://www.amfam.com", "attributes": {"founded": "1967; 55years ago", "numberEmployees": "7,448 (2018)"}}
+{"type": "company", "name": "Lemonade, Inc.", "url": "https://www.lemonade.com/?f=1", "attributes": {"founded": "April2015; 7years ago", "numberEmployees": "1,119 (Dec 2021)"}}
+{"type": "company", "name": "Root, Inc.", "url": "https://www.joinroot.com/", "attributes": {"founded": "March2015", "numberEmployees": "1,241(2022)"}}
+{"type": "company", "name": "General Motors", "url": "https://www.cadillac.com", "attributes": {"founded": "September16, 1908; 113 years ago[1] (original...", "numberEmployees": "157,000 (December 2021)[2]"}}
+{"type": "company", "name": "The Hertz Corporation", "url": "https://en.wikipedia.org/wiki/The_Hertz_Corporation", "attributes": {"founded": "1918; 104years ago in Chicago, Illinois, U.S.", "numberEmployees": "24,000 (2020)"}}
+{"type": "company", "name": "Chrysler", "url": "https://en.wikipedia.org/wiki/Chrysler", "attributes": {"founded": "June6, 1925; 96 years ago", "numberEmployees": "c. 90,000(2019)"}}
+{"type": "company", "name": "State Farm", "url": "https://www.statefarm.com/", "attributes": {"founded": "June7, 1922; 99 years ago[2]", "numberEmployees": "57,672 (2019)[1]"}}
+{"type": "company", "name": "Berkshire Hathaway", "url": "https://www.berkshirehathaway.com/", "attributes": {"founded": "1839; 183years agoCumberland, Rhode Island, U.S.", "numberEmployees": "372,000 (2021)[2]"}}
+{"type": "company", "name": "Progressive Corporation", "url": "https://www.progressive.com/", "attributes": {"founded": "March10, 1937; 85 years ago", "numberEmployees": "43,000+ (2021)"}}
+{"type": "company", "name": "Liberty Mutual", "url": "https://www.libertymutual.com/", "attributes": {"founded": "July1, 1912; 109 years ago (as Massachusetts ...", "numberEmployees": "45,000+(2021)"}}
+{"type": "company", "name": "Allstate", "url": "https://www.allstate.com/", "attributes": {"founded": "April17, 1931; 91 years ago[1]", "numberEmployees": "45,780 (2019)[2]"}}
+{"type": "company", "name": "The Travelers Companies", "url": "https://www.travelers.com/", "attributes": {"founded": "1853; 169years ago (as St. Paul Fire & Marine...", "numberEmployees": "~30,800 (December 2019)[2]"}}
+{"type": "company", "name": "USAA", "url": "https://www.usaa.com/ ", "attributes": {"founded": "June20, 1922; 99 years ago", "numberEmployees": "36,000 (2020)[6]"}}
+{"type": "company", "name": "Farmers Insurance Group", "url": "https://www.farmers.com/", "attributes": {"founded": "1928; 94years agoLos Angeles,[2] California, ...", "numberEmployees": "12,740 (2019)"}}
+{"type": "company", "name": "Nationwide Mutual Insurance Company", "url": "https://www.nationwide.com", "attributes": {"founded": "1926; 96years ago", "numberEmployees": "25391"}}
+{"type": "company", "name": "American International Group", "url": "https://www.aig.com/business", "attributes": {"founded": "December19, 1919; 102 years ago[1]Shanghai, C...", "numberEmployees": "49,600 (2020)"}}
+{"type": "company", "name": "Zurich Insurance Group", "url": "https://www.zurich.com", "attributes": {"founded": "1872; 150years ago", "numberEmployees": "53,535 (2018)[1]"}}
+{"type": "company", "name": "The Hartford", "url": "https://www.thehartford.com", "attributes": {"founded": "1810; 212years ago", "numberEmployees": "~19,500 (2019)"}}
+{"type": "company", "name": "American Family Insurance", "url": "https://www.assurant.com", "attributes": {"founded": "1927; 95years ago", "numberEmployees": "11,307 [4](2017)"}}
+{"type": "company", "name": "Fairfax Financial", "url": "https://www.erieinsurance.comhttps://www.fairfax.ca/", "attributes": {"founded": "1951; 71years ago (as Markel Service of Canada)", "numberEmployees": "44,000 (2019)"}}
+{"type": "company", "name": "W. R. Berkley Corporation", "url": "https://www.berkley.com", "attributes": {"founded": "1967; 55years ago", "numberEmployees": "7,448 (2018)"}}
+{"type": "company", "name": "Markel Corporation", "url": "https://www.markel.com/", "attributes": {"founded": "1930; 92years ago", "numberEmployees": "17,400[2](2019)"}}
+{"type": "company", "name": "Axa", "url": "https://www.axa.com/", "attributes": {"founded": "1816; 206years ago[1]", "numberEmployees": "120,869 (2020)[2]"}}
+{"type": "company", "name": "American Family Insurance", "url": "https://www.amfam.com", "attributes": {"founded": "1927; 95years ago", "numberEmployees": "11,307 [4](2017)"}}
+{"type": "company", "name": "Lemonade, Inc.", "url": "https://www.lemonade.com/?f=1", "attributes": {"founded": "April2015; 7years ago", "numberEmployees": "1,119 (Dec 2021)"}}
+{"type": "company", "name": "Root, Inc.", "url": "https://www.joinroot.com/", "attributes": {"founded": "March2015", "numberEmployees": "1,241(2022)"}}
+{"type": "company", "name": "Munich Re", "url": "https://en.wikipedia.org/wiki/Munich_Re", "attributes": {"founded": "3April 1880; 142 years ago[2]", "numberEmployees": "39,642 (2020)[3]"}}
+{"type": "company", "name": "Swiss Re", "url": "https://en.wikipedia.org/wiki/Swiss_Re", "attributes": {"founded": "19December 1863; 158 years ago", "numberEmployees": "15,401 (end 2019)[2]"}}
+{"type": "company", "name": "Hannover Re", "url": "https://en.wikipedia.org/wiki/Hannover_Re", "attributes": {"founded": "1966; 56years ago", "numberEmployees": "3,218 [1]"}}
+{"type": "company", "name": "General Motors", "url": "https://www.cadillac.com", "attributes": {"founded": "September16, 1908; 113 years ago[1] (original...", "numberEmployees": "157,000 (December 2021)[2]"}}
+{"type": "company", "name": "The Hertz Corporation", "url": "https://en.wikipedia.org/wiki/The_Hertz_Corporation", "attributes": {"founded": "1918; 104years ago in Chicago, Illinois, U.S.", "numberEmployees": "24,000 (2020)"}}
+{"type": "company", "name": "Chrysler", "url": "https://en.wikipedia.org/wiki/Chrysler", "attributes": {"founded": "June6, 1925; 96 years ago", "numberEmployees": "c. 90,000(2019)"}}
+{"type": "company", "name": "State Farm", "url": "https://www.statefarm.com/", "attributes": {"founded": "June7, 1922; 99 years ago[2]", "numberEmployees": "57,672 (2019)[1]"}}
+{"type": "company", "name": "Berkshire Hathaway", "url": "https://www.berkshirehathaway.com/", "attributes": {"founded": "1839; 183years agoCumberland, Rhode Island, U.S.", "numberEmployees": "372,000 (2021)[2]"}}
+{"type": "company", "name": "Progressive Corporation", "url": "https://www.progressive.com/", "attributes": {"founded": "March10, 1937; 85 years ago", "numberEmployees": "43,000+ (2021)"}}
+{"type": "company", "name": "Liberty Mutual", "url": "https://www.libertymutual.com/", "attributes": {"founded": "July1, 1912; 109 years ago (as Massachusetts ...", "numberEmployees": "45,000+(2021)"}}
+{"type": "company", "name": "Allstate", "url": "https://www.allstate.com/", "attributes": {"founded": "April17, 1931; 91 years ago[1]", "numberEmployees": "45,780 (2019)[2]"}}
+{"type": "company", "name": "The Travelers Companies", "url": "https://www.travelers.com/", "attributes": {"founded": "1853; 169years ago (as St. Paul Fire & Marine...", "numberEmployees": "~30,800 (December 2019)[2]"}}
+{"type": "company", "name": "USAA", "url": "https://www.usaa.com/ ", "attributes": {"founded": "June20, 1922; 99 years ago", "numberEmployees": "36,000 (2020)[6]"}}
+{"type": "company", "name": "Farmers Insurance Group", "url": "https://www.farmers.com/", "attributes": {"founded": "1928; 94years agoLos Angeles,[2] California, ...", "numberEmployees": "12,740 (2019)"}}
+{"type": "company", "name": "Nationwide Mutual Insurance Company", "url": "https://www.nationwide.com", "attributes": {"founded": "1926; 96years ago", "numberEmployees": "25391"}}
+{"type": "company", "name": "American International Group", "url": "https://www.aig.com/business", "attributes": {"founded": "December19, 1919; 102 years ago[1]Shanghai, C...", "numberEmployees": "49,600 (2020)"}}
+{"type": "company", "name": "Zurich Insurance Group", "url": "https://www.zurich.com", "attributes": {"founded": "1872; 150years ago", "numberEmployees": "53,535 (2018)[1]"}}
+{"type": "company", "name": "The Hartford", "url": "https://www.thehartford.com", "attributes": {"founded": "1810; 212years ago", "numberEmployees": "~19,500 (2019)"}}
+{"type": "company", "name": "Assurant", "url": "https://www.assurant.com", "attributes": {"founded": "1892; 130years ago", "numberEmployees": "14,250 (2020)[citation needed]"}}
+{"type": "company", "name": "Erie Insurance Group", "url": "https://www.erieinsurance.com", "attributes": {"founded": "April20, 1925; 96 years ago", "numberEmployees": "6,000(2022)"}}
+{"type": "company", "name": "Fairfax Financial", "url": "https://www.fairfax.ca/", "attributes": {"founded": "1951; 71years ago (as Markel Service of Canada)", "numberEmployees": "44,000 (2019)"}}
+{"type": "company", "name": "W. R. Berkley Corporation", "url": "https://www.berkley.com", "attributes": {"founded": "1967; 55years ago", "numberEmployees": "7,448 (2018)"}}
+{"type": "company", "name": "Markel Corporation", "url": "https://www.markel.com/", "attributes": {"founded": "1930; 92years ago", "numberEmployees": "17,400[2](2019)"}}
+{"type": "company", "name": "Axa", "url": "https://www.axa.com/", "attributes": {"founded": "1816; 206years ago[1]", "numberEmployees": "120,869 (2020)[2]"}}
+{"type": "company", "name": "American Family Insurance", "url": "https://www.amfam.com", "attributes": {"founded": "1927; 95years ago", "numberEmployees": "11,307 [4](2017)"}}
+{"type": "company", "name": "Lemonade, Inc.", "url": "https://www.lemonade.com/?f=1", "attributes": {"founded": "April2015; 7years ago", "numberEmployees": "1,119 (Dec 2021)"}}
+{"type": "company", "name": "Root, Inc.", "url": "https://www.joinroot.com/", "attributes": {"founded": "March2015", "numberEmployees": "1,241(2022)"}}
+{"type": "company", "name": "Munich Re", "url": "https://en.wikipedia.org/wiki/Munich_Re", "attributes": {"founded": "3April 1880; 142 years ago[2]", "numberEmployees": "39,642 (2020)[3]"}}
+{"type": "company", "name": "Swiss Re", "url": "https://en.wikipedia.org/wiki/Swiss_Re", "attributes": {"founded": "19December 1863; 158 years ago", "numberEmployees": "15,401 (end 2019)[2]"}}
+{"type": "company", "name": "Hannover Re", "url": "https://en.wikipedia.org/wiki/Hannover_Re", "attributes": {"founded": "1966; 56years ago", "numberEmployees": "3,218 [1]"}}
